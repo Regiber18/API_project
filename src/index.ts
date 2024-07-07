fd1392a52eab0c20ff2e98a729b2f62b3e2fc00b
kull <baguvix.montoya@gmail.com>
2024-07-06 22:19:10 -0600

    commit: cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 
 // Importar rutas de módulos
 import alumnRoutes from './alumn/routes/alumnRoutes';
@@ -9,8 +9,6 @@ import ballotsRoute from './ballot/routes/ballotRoutes';
 import personalRoute from "./personal/routes/personalRoutes"; 
 import classRoutes from "./class/routes/classRoutes"
 import roleRoutes from './role/routes/RoleRoutes';
-
-
 
 // Importar middlewares compartidos
 import { errorHandler } from './shared/middlewares/errorHandler';
@@ -21,22 +19,27 @@ dotenv.config();
 
 // Crear la aplicación de Express
 const app: Application = express();
-const port: number = parseInt(process.env.PORT as string, 10);
+const port: number = parseInt(process.env.PORT as string, 10) || 3000;
 
 // Middleware de análisis del cuerpo
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configuración de CORS
+app.use(cors({
+    origin: 'http://localhost:5173', // Permitir solicitudes desde este origen
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Si necesitas enviar cookies o encabezados de autorización
+}));
+
 // Rutas de los módulos
-
 app.use('/api/alumn', alumnRoutes);
-app.use('/api/ballot', ballotsRoute)
-app.use('/api/personal', personalRoute)
-app.use('/api/class', classRoutes)
-app.use('/role', roleRoutes)
+app.use('/api/ballot', ballotsRoute);
+app.use('/api/personal', personalRoute);
+app.use('/api/class', classRoutes);
+app.use('/role', roleRoutes);
 
-
-//cors
-app.use(cors())
 // Middleware para manejar rutas no encontradas
 app.use(notFoundHandler);
 
