41056f744135a9217caa9f242a6cc14f82edc9e3
kull <baguvix.montoya@gmail.com>
2024-07-19 08:59:49 -0600

    commit

--- src/report/models/Report.ts
+++ src/report/models/Report.ts
@@ -2,6 +2,7 @@ export interface Report {
     report_id: number | null; 
     personal_id: number |null;
     topic: string; 
+    status: boolean;
     created_at: string; 
     created_by: string;
     updated_at: string;
