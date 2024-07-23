245abd32357207148b1afb67afea9037e69ea018
kull <baguvix.montoya@gmail.com>
2024-07-22 20:03:55 -0600

    new commit

--- src/class/routes/classRoutes.ts
+++ src/class/routes/classRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { getClassAll, getClassId, createClass, deleteClass, updateClass} from "../controllers/classController"
 import { authMiddleware } from "../../shared/middlewares/auth";
-import upload from "../../shared/middlewares/uploadMIddleware";
 const classRoutes: Router = Router();
 
 classRoutes.get('/', getClassAll);
 classRoutes.get('/:class_id',authMiddleware, getClassId)
-classRoutes.post('/',upload.single('ballotImage'),createClass); 
+classRoutes.post('/',createClass); 
 classRoutes.put('/:class_id', updateClass)
 classRoutes.delete('/:class_id', deleteClass)
 
