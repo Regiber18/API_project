3f7358f8abcc40779ff413786fae1d036b680120
kull <baguvix.montoya@gmail.com>
2024-08-03 21:55:20 -0600

    change id

--- src/alumn/routes/alumnRoutes.ts
+++ src/alumn/routes/alumnRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getAlumnAll , getAllAlumnIds,getNameComplete, getAlumnId, createAlumn, updateALumn, deleteAlumn,  } from "../controllers/alumnController";
-import { authMiddleware } from '../../shared/middlewares/auth';
+//import { authMiddleware } from '../../shared/middlewares/auth';
 
 const alumnRoutes: Router = Router();
 
 alumnRoutes.get('/', getAlumnAll)
 alumnRoutes.get('/getName', getNameComplete)
 alumnRoutes.get('/geIds', getAllAlumnIds)
-alumnRoutes.get('/:alumn_id', authMiddleware, getAlumnId,)
+alumnRoutes.get('/:alumn_id', getAlumnId,)
 alumnRoutes.post("/", createAlumn)
 alumnRoutes.put("/:alumn_id", updateALumn)
 alumnRoutes.delete("/:alumn_id", deleteAlumn)
