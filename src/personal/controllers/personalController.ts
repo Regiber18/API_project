e392aba2a10f21bcfb1457866525ec9a8ff7c290
kull <baguvix.montoya@gmail.com>
2024-08-05 02:05:42 -0600

    change

--- src/personal/controllers/personalController.ts
+++ src/personal/controllers/personalController.ts
@@ -10,6 +10,7 @@ import puppeteer from 'puppeteer';
 
 const secretKey = process.env.SECRET || "";
 
+// Generar una captura de pantalla desde una URL
 const generateScreenshot = async (url: string, outputPath: string) => {
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/chromium-browser',
@@ -21,10 +22,11 @@ const generateScreenshot = async (url: string, outputPath: string) => {
     await browser.close();
 };
 
+// Obtener todos los registros personales
 export const getPersonalAll = async (_req: Request, res: Response) => {
     try {
         const personal = await PersonalServices.getAllPersonal();
-        if (personal) {
+        if (personal.length > 0) {
             res.status(200).json(personal);
         } else {
             res.status(404).json({ message: 'No se encontraron registros' });
@@ -34,11 +36,12 @@ export const getPersonalAll = async (_req: Request, res: Response) => {
     }
 };
 
+// Obtener un registro personal por ID
 export const getPersonalId = async (req: Request, res: Response) => {
     try {
-        const personals = await PersonalServices.getPersonalById(parseInt(req.params.personal_id, 10));
-        if (personals) {
-            res.status(200).json(personals);
+        const personal = await PersonalServices.getPersonalById(parseInt(req.params.personal_id, 10));
+        if (personal) {
+            res.status(200).json(personal);
         } else {
             res.status(404).json({ message: 'Registro no encontrado' });
         }
@@ -47,23 +50,21 @@ export const getPersonalId = async (req: Request, res: Response) => {
     }
 };
 
+// Crear un nuevo registro personal
 export const createPersonal = async (req: Request, res: Response) => {
     try {
         const newEmployee = await PersonalServices.addPersonal(req.body);
-        if (newEmployee) {
-            res.status(201).json(newEmployee);
-        } else {
-            res.status(404).json({ message: 'Algo salió mal' });
-        }
+        res.status(201).json(newEmployee);
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }
 };
 
+// Actualizar un registro personal existente
 export const updatePersonal = async (req: Request, res: Response) => {
     try {
         const personalId = parseInt(req.params.personal_id, 10);
-        const personalData: Personal = req.body.personalData;
+        const personalData: Partial<Personal> = req.body.personalData;
         const alumnos: AlumnData[] = req.body.alumnos || [];
         const asistencia: { alumn_id: number, attended: boolean }[] = req.body.asistencia || [];
         const urlStrings: string[] = personalData.url || [];  
@@ -76,33 +77,39 @@ export const updatePersonal = async (req: Request, res: Response) => {
 
         for (const url of urlStrings) {
             const screenshotPath = path.join(__dirname, `output-${Date.now()}.png`);
-            await generateScreenshot(url, screenshotPath);
-            const timestamp = Date.now();
-            const pdfPath = path.join(__dirname, `pdfs/pase_de_lista_${timestamp}.pdf`);
-            await PersonalServices.createPDFFromImage(screenshotPath, pdfPath);
-            fs.unlinkSync(screenshotPath);
-            const pdfUrl = `${process.env.URL}:${process.env.PORT}/${pdfPath}`;
-            console.log(pdfUrl);
-            pdfUrls.push(pdfUrl);
+            try {
+                await generateScreenshot(url, screenshotPath);
+                const timestamp = Date.now();
+                const pdfPath = path.join(__dirname, `pdfs/pase_de_lista_${timestamp}.pdf`);
+                await PersonalServices.createPDFFromImage(screenshotPath, pdfPath);
+                const pdfUrl = `${process.env.URL}:${process.env.PORT}/${pdfPath}`;
+                pdfUrls.push(pdfUrl);
+                fs.unlinkSync(screenshotPath); // Eliminar el archivo temporal
+            } catch (error) {
+                console.error(`Error generando PDF para URL ${url}:`, error);
+                // Eliminar el archivo temporal en caso de error
+                if (fs.existsSync(screenshotPath)) {
+                    fs.unlinkSync(screenshotPath);
+                }
+                throw new Error('Error al procesar una de las URLs.');
+            }
         }
 
         // Actualizar los datos personales
         const updatedEmployee = await PersonalServices.modifyPersonal(personalId, { ...personalData, url: pdfUrls }, alumnos, asistencia);
 
         if (updatedEmployee) {
-            // Envía el array de URLs directamente
             res.status(200).json(updatedEmployee);
         } else {
             res.status(404).json({ message: 'Registro no encontrado' });
         }
     } catch (error: any) {
-        console.error('Error updating personal:', error);
+        console.error('Error actualizando registro personal:', error);
         res.status(500).json({ error: error.message });
     }
 };
 
-
-
+// Eliminar un registro personal
 export const deletePersonal = async (req: Request, res: Response) => {
     try {
         const deleted = await PersonalServices.deletePersonal(parseInt(req.params.personal_id, 10));
@@ -116,20 +123,20 @@ export const deletePersonal = async (req: Request, res: Response) => {
     }
 };
 
+// Iniciar sesión
 export const loginPersonal = async (req: Request, res: Response) => {
     const { name, password } = req.body;
     try {
-        const result = await PersonalServices.login(name, password);
+        const token = await PersonalServices.login(name, password);
 
-        if (!result) {
+        if (!token) {
             return res.status(401).json({ message: 'Nombre de usuario o contraseña inválidos' });
         } else {
-            const personal = jwt.verify(result, secretKey) as PersonalPayload;
-            res.setHeader("Authorization", result);
+            const personal = jwt.verify(token, secretKey) as PersonalPayload;
+            res.setHeader("Authorization", token);
             res.setHeader("Access-Control-Expose-Headers", "Authorization");
             res.status(200).json({ message: 'Inicio de sesión exitoso', personal });
         }
-
     } catch (error: any) {
         console.error('Error en inicio de sesión:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
