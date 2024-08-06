4976280393df756c51dcbdef027d6d9b840fde2f
kull <baguvix.montoya@gmail.com>
2024-08-06 00:11:33 -0600

    change

--- src/personal/repositories/PersonalRepository.ts
+++ src/personal/repositories/PersonalRepository.ts
@@ -6,7 +6,7 @@ export class PersonalRepository {
 
     public static async findAll(): Promise<Personal[]> {
         return new Promise((resolve, reject) => {
-            connection.query('SELECT personal_id, name, url, role_id, alumns, class_id lastName FROM Personal', (error: any, results)  => {
+            connection.query('SELECT personal_id, name, url, role_id, alumns, class_id, lastName FROM Personal', (error: any, results)  => {
                 if(error) {
                     reject("error")
                 }else {
